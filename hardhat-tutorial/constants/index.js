f06b1cde1c32a18ac3105c31d1f9e42974425e37
Sunday Mgbogu <digitalsimboja@gmail.com>
2022-03-26 10:22:33 +0100

    Updated the contract

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
-const WHITELIST_CONTRACT_ADDRESS = "0x76084176E1eE73Eb8DF4725eE0D2AA9F2D8e2aF4";
+const WHITELIST_CONTRACT_ADDRESS = "0xD5C28EC3015fc396d241B4167303C99caB8091FF";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-omega-ten.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
--- hardhat-tutorial/contracts/CryptoDevs.sol
+++ hardhat-tutorial/contracts/CryptoDevs.sol
@@ -1,4 +1,4 @@
- // SPDX-License-Identifier: MIT
+  // SPDX-License-Identifier: MIT
   pragma solidity ^0.8.4;
 
   import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
@@ -27,7 +27,7 @@
       // Whitelist contract instance
       IWhitelist whitelist;
 
-      // boolean to keep track of when presale started
+      // boolean to keep track of whether presale started or not
       bool public presaleStarted;
 
       // timestamp for when presale would end
